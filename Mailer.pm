2d663361da5bdc3a4d69185c4004967c3dfea3b2
Geo Pertea <geo.pertea@gmail.com>
2018-03-22 15:07:21 -0400

    removed the 90% pid limit

---
+++ Mailer.pm
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+use strict 'vars';
+use POSIX 'uname';
+use IO::File;
+
+#------- retrieve the local host name
+
+my $thisHost= (&POSIX::uname)[1]; # can't trust HOST envvar under condor, because for getenv=TRUE
+                          # it will either inherit HOST and HOSTNAME from submitter env
+                          # or not set anything for getenv=FALSE
+                          # --> so we use POSIX uname() call here
+my $thisUser= $ENV{LOGNAME} || $ENV{USER} || getpwuid($<);
+chomp($thisHost);
+$thisHost=lc($thisHost);
+my ($thisDomain)=($thisHost=~/^[\w\-]+\.(.+)/);
+
+my $def_domain = $thisDomain;
+$def_domain=~s/^dfci\./jimmy\./;
+my $def_host = $thisHost;
+
+#----------------------
+sub send_mail {
+ my $hash=shift;
+ $hash->{'from'}=$thisUser.'@'.$def_host
+    unless defined($hash->{'from'});
+ my $to=$hash->{'to'};
+ unless ($to) {
+    $hash->{'to'}=$thisUser.'@'.$def_domain;
+    }
+   else {
+    $hash->{'to'}=$to.'@'.$def_domain unless $to =~ m/@/;
+    }
+    
+ my $file;
+ local *ADDFILE;
+ if (defined($hash->{file})) {
+   #warning: assumes it's a decent, short text file!
+   local $/=undef; #read whole file
+   open(ADDFILE, '<'.$hash->{file}) || return "Cannot open file ".$hash->{file}."\n";
+   $file=<ADDFILE>;
+   close ADDFILE;
+   }
+ my $body = $hash->{'body'};
+ $body.=$file;
+ my $fh;
+ $fh = IO::File->new('| sendmail -t -oi') ||
+    die("Mailer.pm error: Cannot open the sendmail pipe\n");
+ $fh->print("To: $hash->{to}\n");
+ $fh->print("From: $hash->{from}\n");
+ $fh->print("Subject: $hash->{subj}\n\n");
+ $fh->print($body);
+ $fh->close();
+ print STDERR "[DEBUG] ======================> mail sent!\n";
+ #-- create the Mail::Mailer object and send the message:
+ # my $mailer = Mail::Mailer->new();
+ # $mailer->open({ 'From'    => $hash->{'from'},
+ #                'To'      => $hash->{'to'},
+ #                'Cc'      => $hash->{'cc'},
+ #                'Subject' => $hash->{'subj'}
+ #              })
+ #    or die "Mailer.pm error: Can't open. $!\n";
+ #print $mailer $body;
+ #$mailer->close();
+}
+
+sub thisHost {
+ return $thisHost;
+}
+
+sub thisUser {
+ return $thisUser;
+}
