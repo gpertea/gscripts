d7534a52b784c4299ff26ac482d5c81c8348843f
Geo Pertea <geo.pertea@gmail.com>
2019-04-02 13:44:16 -0400

    gb2chrnames.pl now can map RefSeq GRCh38 names to either UCSC or Gencode sequence names, for the same patch level

--- gb2chrnames.pl
+++ gb2chrnames.pl
@@ -4,11 +4,12 @@ use Getopt::Std;
 use FindBin;use lib $FindBin::Bin;
 
 my $usage = q/Usage:
- gb2chrnames.pl GRCh_assembly_report.txt hgXX.fa.fai > gb2chr.txt
- 
- Attempts to match the chromosome names from the fasta file
- with the ones from the NCBI assembly report, creating a translation
- table which can be used by gffread's -m option.
+ gb2chrnames.pl GCF_..._assembly_report.txt genome.fa.fai > gb2chr.txt
+
+ Matches the chromosome names from GenBank assembly report to the names found
+ in the given fasta file index (which must be of UCSC or Gencode origin).
+ This will create a 2-column translation table which can be used by 
+ gffread's -m option.
 /;
 umask 0002;
 
@@ -22,66 +23,69 @@ if ($outfile) {
   }
 # --
 
-my %gb2chr; # GB accession => fasta name (multiple GB accs may point to the same chr name)
-my %chr2gb; # fasta chr name => [GBacc1, GBacc2] (multiple GB accs possible)
+my %chr2gb; # chrNum or GB accession => RefSeq accession
+my %chr2gblen; # chrNum or GB accession => RefSeq length
 my @fachr;
-open(CHRNAMES, $ARGV[1]) || die("Error opening file $ARGV[1]\n");
 open(GBTBL, $ARGV[0]) || die("Error opening assembly report file $ARGV[0]");
+open(CHRNAMES, $ARGV[1]) || die("Error opening file faidx file $ARGV[1]\n");
+
 while(<GBTBL>) {
   next if m/^#/;
   my @t=split(/\t/);
   if ($t[1] eq 'assembled-molecule') {
-    if ($t[0] eq 'MT') { #mouse file has Mt chromosome in there
-      $t[0]='M';
-    }
+    $t[0]='M' if lc($t[0]) eq 'mt';
+    print STDERR "Warning: suspicious chromosome name: chr$t[0] !\n"
+       unless ($t[0]=~m/^\d\d/ || $t[0]=~m/[\dXYM]/);
     $chr2gb{'chr'.$t[0]}=$t[6];
+    $chr2gblen{'chr'.$t[0]}=$t[8];
     next;
   }
-  #anything else should be a scaffold:
-  if ($t[1]=~m/\-scaffold/) {
-    my $vname=$t[4];
-    $vname=~tr/./v/;
-    $chr2gb{$vname}=$t[6];
-  }
+  # --anything else should be a one of: 
+  #  alt-scaffold, unlocalized-scaffold, unplaced-scaffold, novel-patch, fix-patch
+  my $gbacc=$t[4];
+  $chr2gb{$gbacc}=$t[6];
+  $chr2gblen{$gbacc}=$t[8];
 }
 close(GBTBL);
 
 #now go through fasta names and match them in %chr2gb
 while (<CHRNAMES>) {
   next if m/^#/;
-  my @t=split(/\t/); # $t[0] = UCSC chr name
-  if ($t[0]=~m/chr[\dXYMUn]{1,2}_([A-Z][A-Z]\d+v\d)/) {
-    my $vacc=$1;
-    my $refacc=$chr2gb{$vacc};
-    if ($refacc) {
-      print "$refacc\t$t[0]\n";
-    }
-    else {
-      print STDERR "WARNING: no GenBank accession match found for $t[0] ($vacc)!\n";
-    }
-  } else { # assembled molecule
-    my $refacc = $chr2gb{$t[0]};
-    if ($refacc) {
-      print "$refacc\t$t[0]\n";
-    }
-    else {
-      print STDERR "WARNING: unrecognized chromosome/scaffold name: $t[0] !\n";
-    }
+  my @t=split(/\t/); # $t[0] = UCSC/Gencode chr name
+  my $gbid; #ID to find in %chr2gb
+  if ($t[0]=~m/^chr[\dXYM][\dTt]?$/) {
+    # assembled molecule (chromosome)
+    $gbid=$t[0];
+    $gbid='chrM' if lc($t[0]) eq 'chrmt';
+  }
+  elsif ($t[0]=~m/^[A-Z][A-Z]\d+\.\d+$/) {
+   # Gencode: plain accession.version
+   $gbid=$t[0];
+  }
+  elsif ($t[0]=~m/chr[\dXYMTUn]{1,2}_([A-Z][A-Z]\d+v\d)/) {
+    $gbid=$1;
+    $gbid=~s/v/./;
+  }
+  else {
+    print STDERR "WARNING: unexpected sequence id format: $t[0]\n";
+    next;
+  }
+  my $racc = $chr2gb{$gbid};
+  if ($racc eq 'na') {
+   print STDERR "Warning: $gbid has 'na' mapping in GenBank sequence data, converting to self-mapping\n";
+   $racc=$gbid;
+  }
+  if ($racc) {
+     print "$racc\t$t[0]\n";
+     print STDERR "WARNING: $racc / $t[0] sequence length mismatch !\n" 
+         if $t[1]!=$chr2gblen{$gbid};
+  } else {
+     print STDERR "WARNING: $gbid chromosome/scaffold name not found!\n";
   }
 }
 close(CHRNAMES);
-
-#foreach my $acc (sort keys(%gb2chr)) {
-# my $chr=$gb2chr{$acc};
-# print "$acc\t$chr\n";
-#}
-
-
 # --
 if ($outfile) {
  select(STDOUT);
  close(OUTF);
- }
-
-#************ Subroutines **************
-
+}
