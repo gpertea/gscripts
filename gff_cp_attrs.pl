b6efb652cee98834960f3015ec8c98efa5e1afe0
Geo Pertea <geo.pertea@gmail.com>
2019-04-09 00:34:42 -0400

    gff_cp_attrs rewritten to transfer CDS/exon features on request

--- gff_cp_attrs.pl
+++ gff_cp_attrs.pl
@@ -4,27 +4,39 @@ use Getopt::Std;
 use FindBin;use lib $FindBin::Bin;
 
 my $usage = q/Usage:
- gff_cp_attrs.pl [-a 'attr_lst'] [ -r 'remove_lst'] attrs_src.gff target.gff
- Copies the additional transcript attributes from attrs_src.gff to the 
- transcripts with the same IDs from target.gff. Exon and CDS features are
- unchanged.
+ gff_cp_attrs.pl [-a 'attr_lst'] [ -r 'remove_lst'] [-E | -C] [-V] \
+   [-m ID.map] src.gff target.gff
+ Transfer the additional transcript attributes from src.gff to the 
+ transcripts with the same IDs from target.gff (or mapped IDs with -m option).
+ Optionally, it can also transfer or replace all exons and\/or CDS in 
+ target.gff with the exons and CDS from src.gff
   
- The output will be a GFF3 which is the same with target.gff but with
- attributes from attrs_src.gff copied over for the matching transcript ID.
+ The output will be a GFF3 which is an "enriched" version of target.gff 
+ with new attributes (or exons\/CDS) added from src.gff
   
  Options:
-  -a denotes a list of attribute names (comma, period or colon delimited)
-     which should be copied over from attrs_src.gff (default is to transfer
-     all new attributes)
-  -r denotes a list of transcript attributes to be discarded from the output
+  -a a list of attribute names (comma, period or colon delimited)
+     which should be transferred from src.gff (default is to transfer
+     only attributes that are not already present in target.gff)
+  -r a list of transcript attributes to be discarded from the GFF output
+  -O override (replace) existing attribute values in target
+  -m instead of looking for a direct match of transcript\/gene IDs, map the
+     target.gff IDs to the src.gff IDs using a mapping file ID.map (with 
+     the target IDs in the 1st column and corresponding src.gff IDs in the 2nd)
   -t override the GFF track column in the output with the given value
   -C checks if there is matching of the boundary and exon definitions 
      of transcripts with the same ID in the two gff input files and 
-     transfer the CDS features from attrs_src.gff to target.gff
+     transfer the CDS features from src.gff to target.gff
+  -E transfer all exon and CDS features from src.gff to target.gff, 
+     completely replacing existing exon\/CDS features in target.gff
+  -V verbose processing (logging the applied changes)
+
+Note: input is expected to be GFF3 "normalized", with full 'exon' and\/or
+'CDS' features (no *codon* or *UTR* features)
 /;
 
 umask 0002;
-getopts('Ct:r:a:o:') || die($usage."\n");
+getopts('OVECt:r:a:m:o:') || die($usage."\n");
 my $outfile=$Getopt::Std::opt_o;
 if ($outfile) {
   open(OUTF, '>'.$outfile) || die("Error creating output file $outfile\n");
@@ -33,10 +45,15 @@ if ($outfile) {
 # --
 my %xcludeAttr= ( 'ID' => 1, 'exonCount'=> 1, 'exons'=> 1, 'CDS'=>1, CDSphase=>1 );
 my %removeAttr;
+my %idmap; # srcID => targetID
 my $CDStransfer=$Getopt::Std::opt_C;
+my $exonReplace=$Getopt::Std::opt_E; #completely replace exon/CDS lines
+my $attrReplace=$Getopt::Std::opt_O;
 my $gfftrack=$Getopt::Std::opt_t;
 my $attrlist=$Getopt::Std::opt_a;
+my $mapfile=$Getopt::Std::opt_m;
 my $removeattrs=$Getopt::Std::opt_r;
+my $verbose=$Getopt::Std::opt_V;
 my %attr;
 if ($attrlist) {
  my @l=split(/[:\.,]/, $attrlist);
@@ -48,17 +65,55 @@ if ($removeattrs) {
 }
 die("${usage}") if @ARGV!=2;
 my $srcgff=shift(@ARGV);
-my %satrs; #ID => { attr => value, ... }
-my %se; #ID => [ [@exons], [@CDS], strand ] 
+my %satrs; #source attributes: ID => { attr => value, ... }
+my %se; #source data: ID => [ [@exons], [@CDS], strand ] 
+          # @exons and @CDS are lists of [start, end, gffline]
+          
+if ($mapfile) {
+  open(MAP, $mapfile) || die("Error opening $mapfile ($!)!\n");
+  while(<MAP>) {
+    next if m/^#/;
+    chomp;
+    my @t=split;
+    $idmap{$t[1]}=$t[0];
+  }
+  close(MAP);
+}
+
 open(SRCIN, $srcgff)||die("Error: cannot open $srcgff! ($!)\n");
 
 while(<SRCIN>) { #source file to copy attributes for each ID
   #just store all ids with their attrs
   my $srcline=$_;
   chomp;
-  my ($chr, $track, $f, $fstart, $fend, $fscore, $strand, $phase, $ats)=split(/\t/);
-  next if m/^#/ || !$ats;
-  my ($id)=($ats=~m/\bID=([^;]+)/);
+  my @t=split(/\t/);
+  if ($gfftrack) {
+    $t[1]=$gfftrack;
+    $srcline=join("\t",@t)."\n";
+  }
+  next if m/^#/ || !$t[8];
+  my ($id)=($t[8]=~m/\bID=([^;]+)/);
+  my ($pid)=($t[8]=~m/\bParent=([^;]+)/);
+  if ($mapfile) {
+   if ($id) {
+      my $tid=$idmap{$id};
+      if ($tid) {
+         $id=$tid;
+         $t[8]=~s/\bID=([^;]+)/ID=$id/;
+         $srcline=join("\t",@t)."\n";
+      }
+   }
+   if ($pid) {
+      my $p=$idmap{$pid};
+      if ($p) {
+         $pid=$p;
+         $t[8]=~s/\bParent=([^;]+)/Parent=$pid/;
+         $srcline=join("\t",@t)."\n";
+      }
+   }
+  }
+  #     0      1     2      3       4      5        6        7      8
+  my ($chr, $track, $f, $fstart, $fend, $fscore, $strand, $phase, $ats)=@t;
   my %ah;
   #my %ah= map { @a=split(/\s*=\s*/);$a[0]=>$a[1] } @astr;
   my @astr=split(/\s*\;\s*/, $ats);
@@ -66,17 +121,14 @@ while(<SRCIN>) { #source file to copy attributes for each ID
     $astr[0]=~s/^\s+//;
     $astr[-1]=~s/\s+$//;
   }
-  #next unless $tid; #ignore lines without ID
-  next unless ($id || $CDStransfer);
+  my $isExon=($f=~m/^(exon|CDS)$/i);
+  my $isCDS=($f=~m/^CDS$/i);
+  #next unless ($id || $CDStransfer || $exonReplace);
   my @alst; #list of attributes stored, in order they were found
   foreach my $avstr (@astr) {
     my ($an, $av)=split(/\s*=\s*/, $avstr);
-    if ($an eq 'ID') {
-      die("Error: parsed ID ($id) not matching attr value for ID ($av)\n")
-         if $id ne $av;
-      next;
-    }
-    
+    next if $an eq 'ID';
+    $av=$pid if $an eq 'Parent';
     if ($attrlist) { #specific attr list given
       next if !exists($attr{$an});
     } else {
@@ -88,39 +140,32 @@ while(<SRCIN>) { #source file to copy attributes for each ID
   }
   #special storing of feature type:
   $ah{'~f'}=$f;
-  #special storing of attribute list in order
+  #special storing of attribute list in order they were found
   $ah{';;'}=[@alst];
   #$id=~s/\.([a-z]+)\d+$//; #trim GMAP suffixes?
   $satrs{$id}={ %ah };
-  #my ($parent)=($ats=~m/\bParent=([^;]+)/);
-  my $parent=$ah{'Parent'};
-  if ($parent && $CDStransfer) {
-    my $ed=$se{$parent}; #exon data for this parent ID
+  #my $parent=$ah{'Parent'};
+  if ($pid && $isExon && ($CDStransfer || $exonReplace)) {
+    my $ed=$se{$pid}; #exon data for this parent ID
     #print STDERR "[DBG]>> trying to add $f \[$fstart, $fend\] to \$se\{$parent\}\n";
-    if ($f=~m/(exon|utr)/i) {
-       unless ($ed) { $ed=[[],[], $strand]; $se{$parent}=$ed; }
-       push(@{$$ed[0]}, [$fstart, $fend]);
-    }
-    elsif ($f=~m/(cds|codon)/i) {
-       unless ($ed) { $ed=[[],[], $strand]; $se{$parent}=$ed; }
-       push(@{$$ed[1]}, [$fstart, $fend, $srcline]);
-       push(@{$$ed[0]}, [$fstart, $fend]); #merge CDS into exons (to be normalized, just in case)
-    }
+    unless ($ed) { $ed=[[],[], $strand]; $se{$pid}=$ed; }
+    push(@{$$ed[ $isCDS ? 1 : 0 ]}, [$fstart, $fend, $srcline]);
   }
-
 }
 close(SRCIN);
-#normalize exon data:
-if ($CDStransfer) {
- foreach my $ed (values (%se)) {
-   normalizeExons($$ed[0]);
- }
-}
+
+#normalize exon data ?
+#if ($CDStransfer || $exonReplace) {
+# foreach my $ed (values (%se)) {
+#   normalizeExons($$ed[0]);
+# }
+#}
+
 # read the target file
 shift(@ARGV) if $ARGV[0] eq '-';
-my @ts; #transcripts or gene IDs, in the order they were encountered
-my %tdata; # id=>[line, [@nexon/CDS_lines], [@exons], [@cds], strand];
-############        0         1[]              2[]     3[]      4
+my @ts; #transcripts or gene IDs, in the order they were found in the input stream
+my %tdata; # id=>[transcript/gene line,  [@exons], [@cds], strand];
+############              0                 1[]     2[]     3
 while (<>) {
   my $line=$_;
   chomp;
@@ -136,12 +181,12 @@ while (<>) {
     $line=join("\t",@t)."\n";
   }
   my @avs=split(/\s*\;\s*/, $t[8]);
-  if (@avs>0) { #should be
+  if (@avs>0) {
     $avs[0]=~s/^\s+//;
     $avs[-1]=~s/\s+$//;
   }
-  my %ah;
-  my @alst;
+  my %ah; #hash of current target attributes
+  my @alst; #list of current target attributes
   foreach my $av (@avs) {
     my ($a, $v)=split(/\s*=\s*/, $av);
     $a=~s/^\s+//;
@@ -152,7 +197,8 @@ while (<>) {
   }
   my $id=$ah{'ID'};
   my $pid=$ah{'Parent'};
-  my $isExon=$pid && $t[2]=~m/(exon|utr|cds|codon)/i;
+  my $isExon=$pid && $t[2]=~m/^(exon|CDS)$/i;
+  my $isCDS=$isExon && $t[2]=~m/^CDS$/i;
   my $td;
   if ($isExon) { #exon or CDS
      $td=$tdata{$pid};
@@ -162,45 +208,41 @@ while (<>) {
            $t[6]=$$sed[2];
            $line=join("\t",@t)."\n";
          }
-         $td=['', [], [], [], $t[6] ]; 
+         $td=['', [], [], $t[6] ]; 
          $tdata{$pid}=$td;
-     } elsif ($td->[4] ne $t[6]) {
-       $t[6]=$td->[4];
+     } elsif ($td->[3] ne $t[6]) {
+       $t[6]=$td->[3];
        $line=join("\t",@t)."\n";
      }
-     if ($CDStransfer) {
-       if ($t[2]=~m/(cds|codon)/i) {
-         push(@{$$td[3]}, [$t[3], $t[4]]);
-         push(@{$$td[2]}, [$t[3], $t[4]]);
-       }
-       else { #exon/UTR
-         push(@{$$td[2]}, [$t[3], $t[4]]);
-       }
+     if ($CDStransfer || $exonReplace) {
+         push(@{$$td[ $isCDS ? 2 : 1 ]}, [$t[3], $t[4], $line]);
      }
-  } else { #non-exon (i.e. transcript or gene)
-    die("Error parsing no-ID line:\n$line\n") unless $id;
-    $td=$tdata{$id};
-    die("Error: ID $id duplicated?\n") if $td;
-    my $sed=$se{$id};
-    if ($sed && $$sed[2] ne $t[6]) {
-       $t[6]=$$sed[2];
-       $line=join("\t",@t)."\n";
-    }
-    $td=['', [], [], [], $t[6] ];
-    $tdata{$id}=$td;
-    push(@ts, $id);
+     next; #exon/CDS lines are stored as they are, replaced later if requested
   }
+  #else { # non-exon (i.e. transcript or gene)
+  die("Error parsing no-ID line:\n$line\n") unless $id;
+  $td=$tdata{$id};
+  die("Error: ID $id duplicated?\n") if $td;
+  my $sed=$se{$id};
+  if ($sed && $$sed[2] ne $t[6]) {
+     $t[6]=$$sed[2];
+     $line=join("\t",@t)."\n";
+  }
+  $td=[$line, [], [], $t[6] ];
+  $tdata{$id}=$td;
+  push(@ts, $id);
+  #}
   #$id=~s/\.([a-z]+)\d+$//; #remove GMAP prefix?
-  my $sa=$satrs{$id} if $id; #source attributes data for this ID (exons too, if they have ID)
+  my $sa=$satrs{$id} if $id; #source attributes data for this ID
   if (!$sa) {
-    #ID data not found in source, just keep it as is
-    if ($isExon) { push(@{$$td[1]}, $line) }
-          else { $$td[0]=$line }
+    ##ID data not found in source, just keep it as is
+    #if ($isExon) { push(@{$$td[1]}, $line) }
+    #      else { $$td[0]=$line }
     next;
   }
-  #important: source feature name should be transferred too, could be different
+  #important: source feature name should be transferred too, could be different!
   $t[2]=delete($sa->{'~f'}) || die("Error: could not find feature type attribute for $id!\n");
-  my $bl=join("\t",@t[0..7])."\tID=$id"; #building line -- with adjusted attributes
+  my $bl=join("\t",@t[0..7])."\tID=$id"; #building the GFF line -- with adjusted attributes
   foreach my $a (@alst) {
     if ($a eq 'ID' 
          || ($removeattrs && exists($removeAttr{$a})) ) {
@@ -209,63 +251,76 @@ while (<>) {
     }
     my $v=$ah{$a};
     my $sv=$sa->{$a};
-    if ($sv) {
+    if (length($sv) && $attrReplace) {
       $bl.=";$a=$sv";
-      delete $sa->{$a}; #so we don't print it later
     }
     else {
       $bl.=";$a=$v"; #print the original value
     }
+    delete $sa->{$a} if length($sv); #so we don't print it later
   }
   #now print the remaining attributes from source
   foreach my $a (@{$sa->{';;'}}) {
-    #next if ($removeattrs && exists($removeAttr{$a}));
+    next if ($removeattrs && exists($removeAttr{$a}));
     my $av=$sa->{$a};
-    $bl.=";$a=$av" if $av;
+    $bl.=";$a=$av" if length($av);
   }
-  if ($isExon) { push(@{$$td[1]}, "$bl\n") }
-         else { $$td[0]="$bl\n" }
+  #if ($isExon) { push(@{$$td[1]}, "$bl\n") }
+  # else { $$td[0]="$bl\n" }
+  $$td[0]="$bl\n";
 } # while <target>
 
+#now for each target gene/transcript, perform any exon/CDS replacements, as requested
+# and print it
 foreach my $tid (@ts) {
    if ($tid=~m/^#[^\n]+\n$/) {
      print $tid;
      next;
    }
    my $td=$tdata{$tid};
-   if ($CDStransfer && @{$$td[2]}>0) {
-      #die("Warning: record ID $tid has no exons?!\n") if (@{$$td[2]}==0);
-      normalizeExons($$td[2]);
-      ## -- now validate if exon structure is matching the source
+   if ($CDStransfer && @{$$td[1]}>0) {
+      #die("Warning: record ID $tid has no exons?!\n") if (@{$$td[1]}==0);
+      #normalizeExons($$td[1]);
+      ## -- for $CDStransfer, check that the exon structure is matching the source
       ##    and if so, transfer CDS
       my $sd=$se{$tid};
       if ($sd) { #only valid if source ID exists
-        if (matchingExons($$td[2], $$sd[0])) {
+        if (matchingExons($$td[1], $$sd[0])) {
             if (@{$$sd[1]}>0) {
-              print STDERR "WARNING: existing target CDS override for $tid\n"
-                if (@{$$td[3]}>0);
-              #@{$$td[3]}=@{$$sd[1]};
-              #now we MUST delete any existing CDS lines in $$td[1]
-              my @nocds=grep { !m/^\S+\t[^\t]+\t(\w*CDS\w*|\w*codon)\t/i } @{$$td[1]};
-              foreach my $cd (@{$$sd[1]}) {
-                 if ($gfftrack) {
-                   my @t=split(/\t/, $$cd[2]);
-                   $t[1]=$gfftrack;
-                   $$cd[2]=join("\t",@t);
-                 }
-                 push(@nocds, $$cd[2]); #print line as stored in the source
-              }
-              @{$$td[1]}=@nocds;
+              print STDERR "WARNING: existing CDS override for $tid\n"
+                if (@{$$td[2]}>0);
+              @{$$td[2]}=@{$$sd[1]};
             }
         }
-        else { print STDERR "WARNING: exons structure mismatch for $tid!\n" }
+        else { print STDERR "WARNING: exons structure mismatch for $tid, CDS not changed!\n" }
        #} #else {
        #print STDERR "[DBG]>> Warning: no source data found for $tid\n";
       }#source ID to compare to
    } #-- if $CDStransfer
+   elsif ($exonReplace) {
+       my $sd=$se{$tid};
+       if ($sd) { 
+         print STDERR "WARNING: exons replaced for $tid\n";
+         @{$$td[1]}=@{$$sd[0]};
+         my $cop;
+         if (@{$$sd[1]}>0) {
+           $cop=(@{$$td[2]}>0) ? 'replaced' : 'added';
+         } elsif (@{$$td[2]}>0) {
+            $cop='removed!';
+         }
+         if ($cop) {
+           @{$$td[2]}=@{$$sd[1]};
+           print STDERR "WARNING: CDS $cop for $tid\n";
+         }
+       }
+   }
+   # -- print target GFF record and its exons/CDS data
    print $$td[0];
    foreach my $el (@{$$td[1]}) {
-     print $el;
+      print $el->[2];
+   }
+   foreach my $cl (@{$$td[2]}) {
+      print $cl->[2];
    }
 } #for each target ID
 
