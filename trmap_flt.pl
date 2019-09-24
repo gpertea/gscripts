1d8fa41db11c2ee9d2c4364dc5da6e5a9652985b
Geo Pertea <geo.pertea@gmail.com>
2019-09-24 14:55:19 -0400

    added more CDS partial matching hints

--- trmap_flt.pl
+++ trmap_flt.pl
@@ -20,7 +20,9 @@ Options:
  -S     only show overlaps with single-exon reference transcripts
  -M     only show overlaps with multi-exon reference transcripts
  -t     output a simple 3 column table format (tab delimited): 
-        query_id, ovl_code, reference_id
+          qry_id  qryExonCount ovl_code ref_id refExonCount [CDSmatch]
+        (the CDSmatch column is only present if trmap was run with --show-cds
+        and it will be T if CDS coordinates match or F otherwise
 /;
 
 die($usage."\n") if (@ARGV==0 || $ARGV[0] eq '-h' || 
@@ -37,48 +39,68 @@ my $qmet=$Getopt::Std::opt_m;
 my $codes=shift(@ARGV);
 
 my %c= map { $_=>1 } (split(//,$codes));
-my ($h, $hpr, $qid);
+my ($h, $hpr, $qid, $qxn, $qcd, $qcstart, $qcstop);
 my $skip;
 while (<>) {
  my $l=$_;
  if (m/^>/) {
-   # header : query transcript data
+   # header line
+   # query transcript data: tid, loc, strand, exons, [CDS:start:end]
    $skip=0;
    ($h, $hpr) = ($_, 1);
-   if ($tblout) {
-     ($qid)=(m/^>(\S+)/);
+   chomp;
+   my ($qloc, $qs, $qx);
+   ($qid, $qloc, $qs, $qx, $qcd)=split(/\s+/);
+   $qid=substr($qid,1);
+   if ($qcd && $tblout) {
+     ($qcstart)=($qcd=~m/^[CDS:]*(\d+)/);
+     ($qcstop)=($qcd=~m/(\d+)$/);
+     ($qcstart, $qcstop)=($qcstop, $qcstart) if ($qs eq '-');
    }
+   $qxn=scalar(split(/\,/,$qx));
    if ($qset || $qmet) {
      chomp;
-     my ($x)=(m/([\d\-\,]+)$/);
-     my $qc=scalar(split(/\,/,$x));
      if ($qset) {
-       $skip=($qc>1);
+       $skip=($qxn>1);
      } else { #multi-exon only
-       $skip=($qc==1);
+       $skip=($qxn==1);
      }
    }
  }
- else {
+ else { #ref hit line:
+   #  ovlCode, chr, strand, start, end, rID, exons, [CDS:start:end]
    next if $skip;
    #overlapped reference info
-   my @d=split(/\t/);
-   if (exists($c{$d[0]})) { #code match
-     if ($rset || $rmet) {
-       chomp;
-       my ($x)=(m/([\d\-\,]+)$/);
-       my $rc=scalar(split(/\,/,$x));
-       if ($rset) {
-         next if $rc>1;
-       } else { #multi-exon only
-         next if $rc==1;
-       }
-     }
-     if ($tblout) {
-        print join("\t",$qid, $d[0], $d[5])."\n";
-        next;
-     }
-     if ($hpr) {
+   chomp;
+   my ($oc, $chr, $rs, $r0, $r1, $rid, $rx, $rcd)=split(/\t/);
+   if (exists($c{$oc})) { #ovl code match
+   my $rxn=scalar(split(/\,/,$rx));
+   my ($rcstart, $rcstop);
+   if ($rset || $rmet) {
+      if ($rset) {
+        next if $rxn>1;
+      } else { #multi-exon only
+        next if $rxn==1;
+      }
+   }
+   if ($tblout) {
+      my $cm='.';
+      if ($rcd && $qcd) {
+        $cm='same_CDS' if $rcd eq $qcd;
+        if ($cm eq '.') {
+           ($rcstart)=($rcd=~m/[CDS:]*(\d+)/);
+           ($rcstop)=($rcd=~m/(\d+)$/);
+           ($rcstart, $rcstop)=($rcstop, $rcstart) if ($rs eq '-');
+        }
+        $cm='same_START' if ($rcstart && $rcstart==$qcstart);
+        if ($rcstop && $rcstop==$qcstop) {
+           $cm= (length($cm)>1) ? $cm.',same_STOP' : 'same_STOP';
+        }
+      }
+      print join("\t",$qid, $qxn, $oc, $rid, $rxn, $cm)."\n";
+      next;
+   }
+   if ($hpr) {
        print $h;
        $hpr=0;
      }
