3ebafd255b4436e5b357de9f05b0623e2a9f91a4
Geo Pertea <geo.pertea@gmail.com>
2019-12-22 22:39:54 -0500

    stringtie_gtex.sh now uses relative paths and recognizes CRAM

--- stringtie_gtex.sh
+++ stringtie_gtex.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
-guides='/home/gpertea/GTEx_redo/refseq_vs_gencode_intersection.prot_lnc.no_alts.clean.gff'
+guides=genome/ref_ann.gff
+ref=genome/ref_genome.fa
+outdir=stringtie
 if [[ -z "$1" || $1 = '-h' || $1 = '--help' ]]; then
  echo "Usage: "
- echo "    stringtie_gtex.sh /full/path/to/tissue/SRAxxxxxx.bam"
- echo "Warning: must be run in the base output directory!"
- echo "Using guides: $guides"
+ echo "    stringtie_gtex.sh path/to/tissue/SRAxxxxxx.bam|cram"
+ echo "Must be run in the base directory, using $outdir/<tissue> as output directory!"
+ echo "Using guides: $guides, reference genome $ref"
  exit 1
 fi
+if [[ ! -d $outdir ]]; then
+  echo "Error: folder $outdir not found!"
+  exit 1
+fi
+
+if [[ ! -f $guides ]]; then
+  echo "Error: reference annotation file $guides not found!"
+  exit 1
+fi
+
+
 fp="$1"
 fn="${fp##*/}"
 if [ ! -s $fp ]; then
-   echo "Error: file empty or not found: $fp"
+   echo "Error: alignment file empty or not found: $fp"
    ls -l $fp
    exit 1
 fi
 tissue="${fp%/*}"
 tissue="${tissue##*/}"
+ext="${fn##*.}"
 sra="${fn%.*}"
-echo "Tissue is <$tissue> , SRA accession is <$sra>"
-if [[ ! -d "$tissue" ]]; then
-  mkdir -p "$tissue"
+#echo "Tissue is <$tissue>, SRA accession is <$sra>"
+if [[ ! -d "$outdir/$tissue" ]]; then
+  mkdir -p "$outdir/$tissue"
 fi
 
 # run stringtie now
-cd "$tissue"
-stringtie -p2 -o $sra.gtf -G $guides $fp > $sra.stringie_err.log 2>&1
+if [[ $ext == "bam" ]]; then
+(stringtie -p2 -o $outdir/$tissue/$sra.gtf -G $guides $fp) > $outdir/$tissue/$sra.stringie_err.log 2>&1
+else #cram assumed
+ (samtools view -uh -T $ref $fp | stringtie -p2 -o $outdir/$tissue/$sra.gtf -G $guides --bam -) > $outdir/$tissue/$sra.stringie_err.log 2>&1
+fi
 
