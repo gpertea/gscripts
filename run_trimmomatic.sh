303ad05c8758e315acd545db9fc06d5ecea41f44
Geo Pertea <geo.pertea@gmail.com>
2021-03-02 21:11:05 -0500

    trimmomatic script

---
+++ run_trimmomatic.sh
@@ -0,0 +1,86 @@
+#!/bin/bash -x
+prog="/opt/Trimmomatic-0.39/trimmomatic-0.39.jar"
+adir="${prog%/*}/adapters"
+if [[ ! -f $prog ]]; then
+  echo "Error: path $prog is invalid!"
+  exit 1
+fi
+
+read -r -d '' USAGE << EOM
+Usage:
+ run_trimmomatic.sh [-S] [-l NN] ..files to trim..
+ 
+Options:
+  -S      single end reads (using TruSeq3-SE.fa adapters)
+          default: paired end reads (using TruSeq3-PE-2.fa)
+
+  -l  NN  specify the minimum length for a read (default:75)
+EOM
+tmode="PE"
+adfile="TruSeq3-PE-2.fa"
+args="2:30:10:1:TRUE"
+qargs="LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:75"
+while getopts "hSl:" OPT; do
+  case $OPT in
+  h)
+    echo "$USAGE"
+    exit 1
+    ;;
+  S)
+    tmode="SE"
+    adfile="TruSeq3-SE.fa"
+    args="2:30:10"
+    ;;
+  l)
+    qargs="LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:$OPTARG"
+    ;;
+  *)
+    echo "$USAGE"
+    echo "Error: incorrect options provided"
+    exit 1
+    ;;
+   esac
+done
+
+shift $((OPTIND-1)) #shift $@
+f1="$1"
+f2="$2"
+fouts=""
+if [[ "$tmode" == "SE" ]]; then
+  #single argument expected
+  if [[ $# -ne 1 ]]; then
+    echo "Error: a single file argument is expected!"
+    exit 2
+  fi
+  if [[ ! -f $f1 ]]; then
+     echo "Error: $f1 not found!"
+     exit 2
+  fi
+  fouts="${f1%.f*q*}_trimmed.fastq.gz"
+else # PE
+  #two arguments expected
+  if [[ $# -ne 2 ]]; then
+    echo "Error: two arguments expected!"
+    exit 2
+  fi
+  if [[ ! -f $f1 || ! -f $f2 ]]; then
+     echo "Error: $f1 or $f2 not found!"
+     exit 2
+  fi
+  f1o="${f1%.f*q*}_trimmed_paired_1.fastq.gz"
+  f2o="${f2%.f*q*}_trimmed_paired_2.fastq.gz"
+  f1un="${f2%.f*q*}_trimmed_unpaired1.fastq.gz"
+  f2un="${f2%.f*q*}_trimmed_unpaired2.fastq.gz"
+  fouts="$f1o $f1un $f2o $f2un"
+fi
+
+java -Xmx512M \
+ -jar $prog $tmode \
+ -threads 4 -quiet \
+ -phred33 -trimlog "${f1%.f*q*}.trim.log" \
+ -summary "${f1%.f*q*}.summary.txt" \
+ $@ \
+ $fouts \
+ "ILLUMINACLIP:$adir/$adfile:$args" \
+ $qargs
+ 
