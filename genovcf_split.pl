00eb5b7737b1a48d14250f8f09bfcd413d743428
Geo Pertea <geo.pertea@gmail.com>
2022-03-24 22:29:23 -0400

    wip genovcf_split

--- genovcf_split.pl
+++ genovcf_split.pl
@@ -29,26 +29,68 @@ if ($brf && -f $brf) {
  close(M)
 } else { $brf=0 }
 
-my $fh = IO::Zlib->new($fin) || die("Error opening $fin\n");
-my $cs=0; #count samples written
-my $hdr; #keep common header lines here
-my $fdata;
+my $fh = IO::Zlib->new($fin, 'rb') || die("Error opening $fin\n");
+my $hdr=''; #keep common header lines here
+my @fws; #array of writing file handles
 my @samples; 
 while (<$fh>) {
-  if (@samples) {
+  if (@samples>0) {
    ## reading data
-   exit(0); #for now
+   closeAll();exit(0); #for now
+      
    next
   }
   ## still in header lines
-  if (m/^#CHROM\S+/) { #column headers, we get sample IDs here
+  if (m/^#CHROM\s+/) { #column headers, we get sample IDs here
+    print STDERR "header line found!\n";
     chomp;
     my @t=split(/\t/);
-    @samples=@t[9..$#t];
-    print STDERR "Found $#samples samples.\n";
-    ##TODO: write header here!
+    @samples=@t[9 .. $#t];
+    print STDERR "Found ".scalar(@samples)." samples. (".scalar(@t)." cols)\n";    
+    ## write header here and sample list
+    open(H, ">$pre.header.vcf") || die("Error writing header file\n");
+    print H $hdr;
+    close(H);
+    open(SL, ">$pre.samples.lst") || die("Error writing samples lst\n");
+    print SL join("\n", @samples)."\n";
+    close(SL);
+    # -- create the directories and open all files for write:
+    my $cs=0; #count samples written
+    my @dnrange=(1, $n);
+    $dnrange[1]=scalar(@samples) if $n>@samples;
+    my $dir=join("_", $pre, @dnrange);
+    mkdir($dir);
+    foreach my $sid (@samples) {
+      $cs++;
+      if ($cs>1 && ($cs % $n) == 1) { #next dir
+        @dnrange=($cs, $cs+$n-1);
+        $dnrange[1]=scalar(@samples) if $n>@samples;
+        $dir=join("_", $pre, @dnrange);
+        mkdir($dir);
+      }
+      my $sid=$samples[$cs-1];
+      my $fn="$dir/$sid".'.vcf.gz';
+      my $fw=IO::Zlib->new($fn, 'wb') || die "Error: cannot create $fn : $!";
+      print $fw "##fileformat=VCFv4.1\n";
+      print $fw join("\t", '#CHROM', 'POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT',$sid)."\n";
+      push(@fws, $fw);
+    }
     next
   }
   next if m/^##bcftools/;
   $hdr.=$_;
+}
+
+$fh->close;
+
+
+
+##======================================================
+
+sub closeAll {
+  print STDERR "closing files...\n";
+  $fh->close;
+  foreach my $fw (@fws) {
+     $fw->close;
+  }
 }
