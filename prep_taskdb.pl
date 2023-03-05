1af0aedb5369115d11f0bac5fa388a4239b743c9
Geo Pertea <geo.pertea@gmail.com>
2023-03-05 17:49:41 -0500

    added prep_rnums_taskdb.pl

--- prep_taskdb.pl
+++ prep_taskdb.pl
@@ -1,112 +1,102 @@
 #!/usr/bin/perl
+### input: arg1 : FASTQ base directory - relative to current directory 
+##                                      OR to ~/work/cbrain/fastq/
+##         arg2 (optional) - path to phenodata file, default:
+##                           ~/work/cbrain/phenodata/libd_samples_phenodata.tab
+##              if '<dataset>:' prefix is found, it is expected to match
+##                             the first column in the phenodata file !
+
+### outputs a CHESS-brain taskdb.tfa with the following format:
+# >3 dataset_fastq_dir mate1_filemask sex
+### which means [1]=FASTQ directory, [2]=mate1 file mask, [3]= M or F
+### Expects:
+#### dataset_fastq_dir : relative to ~/work/cbrain/fastq/ or current directory
+#### mate1_filemask :   sampleID_1.fastq.gz
+####                 or RNum_flowcell_*_R1_*.fastq.gz
+
+### requires: ~/work/cbrain/phenodata/libd_samples_phenodata.tab
+##            (unless arg2 is provided)
 use strict;
-use Getopt::Std;
-##
-my $usage = q/Usage:
-  prep_taskdb.pl [-p pheno_data.tab] fastq_basedir
-  
-  Generate a pseudo-fasta records of the format:
-
->234 sampleID F
-path sampleID_r1.fq.gz sampleID_r2.fq.gz
-
-The sex (M or F) will only follow the record number and 
-sampleID if a pheno_data.tab file was provided with -p
+my $usage=q/
+ prep_taskdb.pl <FASTQ_basepath> [[dataset:]<phenodata_file>]
 /;
-umask 0002;
-getopts('p:o:') || die($usage."\n");
-die($usage."\nError: fastq directory missing ($ARGV[0])!\n") 
-  if (@ARGV==0 || ! -d $ARGV[0]); 
-my $outfile=$Getopt::Std::opt_o;
-my $pdfile=$Getopt::Std::opt_p;
-if ($outfile) {
-  open(OUTF, '>'.$outfile) || die("Error creating output file $outfile\n");
-  select(OUTF);
-}
-my $fdir=$ARGV[0];
-my %pd; # sample_id => [subj_id, dx, sex, age, race]
-my $pdat=0;
-if ($pdfile && -f $pdfile) {
-  open(PD, $pdfile) || die("Error opening $pdfile\n");
-  #header expected:
-  $_=<PD>; chomp;
-  my @h=split(/\t/);
-  #find sex column
-  my ($sidx) = grep { lc($h[$_]) eq 'sex' } (0 .. $#h);
-  die("Error: Sex column not found!\n") unless defined $sidx;
-  my ($dxidx) = grep { lc($h[$_]) eq 'dx' } (0 .. $#h);
-  die("Error: Dx column not found!\n") unless defined $dxidx;
-  my ($ageidx) = grep { lc($h[$_]) eq 'age' } (0 .. $#h);
-  die("Error: Age column not found!\n") unless defined $ageidx;
 
-  my ($raceidx) = grep { lc($h[$_]) eq 'race' } (0 .. $#h);
-  die("Error: Race column not found!\n") unless defined $raceidx;
-  ## assume first column is sample_id, 2nd is subj_id
-  while(<PD>) {
-    chomp;
-    my @t=split(/\t/);
-    $pd{$t[0]}=[ @t[1, $dxidx, $sidx, $ageidx, $raceidx] ];
+my $HOME=$ENV{'HOME'};
+my $basedir="$HOME/work/cbrain"; #base project directory
+my ($fdir, $pdf)=@ARGV;
+die("$usage\n") unless $fdir;
+my $dataset;
+if ($pdf) {
+  my @fds=split(/:/, $pdf);
+  ($dataset, $pdf)=@fds if ($fds[0] ne $pdf); # dataset given
+  $dataset=lc($dataset);
+}
+$pdf="$basedir/phenodata/libd_samples_phenodata.tab" unless $pdf;
+
+my %pd; # SAMPLE_ID => [subj, region, dx, sex, race, age]
+                      #   0       1    2    3    4     5
+my $libd_pd=($pdf=~m/libd_samples_phenodata/);
+
+die("$usage\n") unless -f $pdf;
+my $fpath=$fdir;
+my $cbpath;
+if (! -d $fpath) { #not a relative path to current dir?
+  $fpath="$basedir/fastq/$fdir";
+  $cbpath=1; # bool flag for absolute fqs paths
+  die("Error: path $fpath not found!\n") unless -d $fpath;
+}
+my @fqs=glob("$fpath/*.f*q.gz");
+if (@fqs<2) {
+  @fqs=glob("$fpath/*/*.f*q.gz");
+  die("Error: cannot find FASTQ gz files under $fpath!\n") unless @fqs>=2;
+}
+
+open(PD, $pdf) || die("Error opening $pdf\n");
+while (<PD>) {
+  next if $.==1 && m/(dx|age)/i; # skip header
+  chomp;  my @f=split(/[\s\,]/); 
+  if ($libd_pd) {
+    next if ($dataset && lc($f[0]) ne $dataset);
+    ## default libd phenodata fields
+    ## dataset rnum sample_id region protocol rin p_id dx sex race age pmi
+    ## 0        1       2       3        4     5   6    7  8   9    10 11
+    $pd{$f[1]}=[@f[6,3,8,7, 9,10]];
+  } else {
+    #for anything else but default LIBD phenodata: sample_id, subj_id, region, dx, sex, race, age
+    #                                                 0        1         2     3    4    5     6
+    $pd{$f[0]}=[@f[1,2,4,3,5,6]]; # p_id, region, sex, dx, race, age
   }
-  close(PD);
-  $pdat=scalar(%pd);
 }
-
-# --
-my $cmd="find $fdir -name '*.gz'";
-#print STDERR " running: $cmd\n";
-my @ffns=sort(split(/\n/, `$cmd`));
-#print join("\n", @ffns)."\n";
-my ($prevd, $prevf)=('','');
-my $c=1;
-foreach my $fp (@ffns) {
-  chomp($fp);
-  my ($d, $f)=($fp=~m{(.+)/([^/]+)$});
-  utf8::downgrade($f);
-  if ($d eq $prevd && length($f)==length($prevf)) {
-    my $xor = ($f ^ $prevf);
-    my @r;
-    if ((@r=($xor=~m/[^\0]/g))==1) {
-       my $p=$-[0];
-       if (substr($prevf, $p, 1) eq '1' && substr($f, $p, 1) eq '2') {
-         my ($r1, $r2)=(substr($f, 0, $p+1), substr($f, 0, $p+1));
-         $r1=~s/[_\.]r?[12]$//i; 
-         $r2=~s/[_\.]r?[12]$//i;
-         die("Error: could not get sample ID from $r1 vs $r2\n") 
-             unless $r1 eq $r2;
-         my $sid=$r1;
-         if ($pdat) {
-           #my ($r1, $r2)=($prevf, $f);
-           #$r1=~s/^([^\.]+).+/$1/;
-           #$r2=~s/^([^\.]+).+/$1/;
-           #if ($r1 ne $r2) {
-             # remove _r?[12] at the end to get the sample ID
-           #}
-           my $sd=$pd{$sid};
-           die("Error: could not get pheno data for $r1\n") unless $sd;
-           my $s=$$sd[2];
-           if ($s=~m/^([MF])/i) {
-             $s=uc($1);
-           } elsif ($s=~m/^[XY]+$/) { 
-             $s = ($s =~ m/Y/) ? 'M' : 'F';
-           } else { die("Error: cannot recognize sex : $s\n"); }
-           print ">$c $sid $s\n".join(" ", $d, $prevf, $f)."\n";
-         } else {
-           print ">$c $sid\n".join(" ", $d, $prevf, $f)."\n";
-         }
-        $c++;
-      }
-    }
-    
+close(PD);
+my $rn=1;
+my %sproc; ## processed sample IDs : sampleID => 1
+foreach my $fq (@fqs) {
+  if ($cbpath) { #absolute path
+    substr($fq, 0, length("$basedir/fastq/"), ''); #remove base path
   }
-  $prevf=$f;
-  $prevd=$d;
+  my ($fn)=($fq=~m{([^/]+)$});
+  my $fqdir=$fq;
+  $fqdir=~s{/[^/]+$}{}; # relative path to fastq file to write
+  my ($sid, $rnum, $fc, $spd, $fqmask);
+  if ($fn=~m/^(R\d+)_([A-Z,0-9]+XX)_.+_R[12]_.+/) {
+    ($rnum, $fc)=($1,$2);
+    $sid="${rnum}_$fc";
+    $spd=$pd{$rnum}  || die("Error retrieving phenodata for $rnum ($fn)\n");
+    $fqmask="${sid}_*_R1_*.f*q.gz";
+  } elsif ($fn=~m/(.+)_[12]\.f[ast]*q\.gz$/) {
+    $sid=$1;
+    $spd=$pd{$sid} || die("Error retrieving phenodata for $sid ($fn)\n");
+    $fqmask="${sid}_1.f*q.gz";
+  }
+  unless ($sid) {
+    print STDERR "Warning: cannot parse sampleID from $fn!\n";
+    next;
+  }
+
+  if (!exists($sproc{$sid})) {
+     $sproc{$sid}=1;
+     my ($subj, $region, $sex, $dx, $race, $age)=@$spd;
+     print ">$rn $fqdir $fqmask $sex $dx $race $age\n";
+     $rn++;
+  }
 }
-
-# --
-if ($outfile) {
- select(STDOUT);
- close(OUTF);
- }
-
-#************ Subroutines **************
-
