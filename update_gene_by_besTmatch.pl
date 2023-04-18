3f54c801d5b376345dcf0daa2954bcf3b9c0a1c4
Geo Pertea <geo.pertea@gmail.com>
2023-04-17 21:40:06 -0400

    update gene and jx info

--- update_gene_by_besTmatch.pl
+++ update_gene_by_besTmatch.pl
@@ -4,12 +4,13 @@ use Getopt::Std;
 use FindBin;use lib $FindBin::Bin;
 
 my $usage = q{Usage:
-  update_gene_by_besTmatch.pl trmap_TB.tab file.gtf/gff
+  update_gene_by_besTmatch.pl trmap_TB.ttab trmap_J.jtab tofix.gtf/gff
 
   Change gene_id and gene_name to match those of best overlapping
-  entries in the given trmap -TB output file.
+  entries in the given trmap -TB and -J output files
   
   To be used to bring refseq added entried into Gencode annotation and gene namespace
+  or to just assign a gene_id/gene_name to an arbitrary transfrag file
 };
 umask 0002;
 getopts('o:') || die($usage."\n");
@@ -21,10 +22,12 @@ if ($outfile) {
 # --
 die($usage."\n") if @ARGV==0 || $ARGV[0]=~m/^\-+h/;
 my $tmap=shift(@ARGV);
-die("${usage} Error: input file expected!\n") unless $tmap && $tmap;
+die("${usage} Error: input tmap file expected!\n") unless $tmap && -f $tmap;
+my $jtab=shift(@ARGV);
+die("${usage} Error: input jtab file expected!\n") unless $jtab && -f $jtab;
 
 my %tr; ## tid=>[ gene_name, gene_id ]
-
+my %tj; ## tid=> novel_junction_info
 open(T, $tmap) || die("Error opening file $tmap\n");
 while (<T>) {
   my @t=split(/\t/);
@@ -36,13 +39,21 @@ while (<T>) {
   }
 }
 close(T);
+open(J, $jtab) || die("Error opening file $jtab\n");
+while (<J>) {        #  0    1        2                     3          4
+  chomp;
+  my @t=split(/\t/); # tid exons codes|refTx|ref_gnames, ovl_genes,  njx_info,
+  $tj{$t[0]} = ($t[4] eq '.') ? '' : $t[4];
+}
+close(J);
+
 my $gtf=0;
 
 # read/transform gff/gtf
 while (<>) {
   if (m/^#/) { print $_;next }
   chomp;
-  if (m/ID=([^;]+)/) {
+  if (m/\bID=([^;]+)/) {
   # GFF
     my $t=$1;
     if (my $td=$tr{$t}) {
@@ -56,6 +67,9 @@ while (<>) {
       $repl |= s/geneID=([^;]+)/geneID=$gid;ogeneID=$1/; 
       s/;?$/;geneID=$gid/ unless $repl;
     }
+    if (my $jd=$tj{$t}) {
+      s/;?$/;njx=$jd/;
+    }
   } elsif (m/transcript_id\s+"([^"]+)/) { 
   # GTF
     my $t=$1;
@@ -68,7 +82,11 @@ while (<>) {
          s/;?$/;gene_name "$gn"/;
       }
     }
+    if (m/\ttranscript\t/ && (my $jd=$tj{$t})) {
+      s/;?$/;njx "$jd"/;
+    }
   }
+
   print "$_\n";
 }
 
