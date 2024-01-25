b36586e49187afa9188fdcd6d0d3b05696b7f1e8
Geo Pertea <geo.pertea@gmail.com>
2024-01-25 17:05:55 -0500

    adding make_tx_table.sh

---
+++ make_tx_table.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+fgff=$1
+if [[ -z $fgff ]]; then
+ echo "Error: input GTF/GFF required!"
+ exit 1
+fi
+if [[ ! -f $fgff ]]; then
+  echo "$fgff file not found!\n"
+  exit 1
+fi
+tabcols='@id,@chr,@start,@end,@strand,@numexons,@covlen,@cdslen,@track,@geneid,gene_name,gene_type,tag'
+ftab=${fgff/.gtf/.tab}
+if [[ $ftab == $fgff ]]; then
+ ftab=${fgff/.gff/.tab}
+fi
+echo "$tabcols" |  tr -d '@'  | tr "," "\t" > $ftab
+#zcat $fgff | gffread --table $tabcols >> $ftab
+cat $fgff | gffread --table $tabcols >> $ftab
